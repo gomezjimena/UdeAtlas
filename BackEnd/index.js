4ffc15bb8f27ad23898174e9aca6cf57c62f0979
Gómezjimena <jimena.munoz@udea.edu.co>
2025-11-08 22:09:14 -0500

    Eliminar doble declaracion de variable

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -7,8 +7,6 @@ const app = express();
 const prisma = new PrismaClient();
 const PORT = process.env.PORT || 3001;
 
-const cors = require('cors');
-
 app.use(cors({
   origin: [
     'https://ude-atlas.vercel.app',
