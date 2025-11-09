a0177ddf8459ae973b88fcc04a2192d575598dfa
Gómezjimena <jimena.munoz@udea.edu.co>
2025-11-08 21:13:51 -0500

    Ajuste inicial para render

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -7,6 +7,12 @@ const app = express();
 const prisma = new PrismaClient();
 const PORT = process.env.PORT || 3001;
 
+const cors = require('cors');
+app.use(cors({
+  origin: ['http://localhost:8080', 'http://localhost:3000'],
+  credentials: true
+}));
+
 // Middleware
 app.use(cors());
 app.use(express.json());
